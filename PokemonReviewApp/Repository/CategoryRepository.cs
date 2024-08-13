31a391abacfddeb6740e51f85bdd764a8e144e7b
Janis <janis.goldmanis@gmail.com>
2024-08-14 00:27:52 +0300

    Add project files.

---
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private DataContext _context;
+        public CategoryRepository(DataContext context)
+        {
+            _context  = context;
+        }
+
+        public bool CategoryExists(int categoryId)
+        {
+            return _context.Categories.Any(c => c.Id == categoryId);
+        }
+
+        public bool CreateCategory(Category category)
+        {
+            //Change Tracker
+
+            // add, updating, modifying
+            // connected vs disconected
+            // EntityState.Added = disconnected state
+            _context.Add(category);
+            return Save();
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(e=> e.CategoryId == categoryId).Select(c => c.Pokemon).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+    }
+}
---
+++ PokemonReviewApp/Repository/CountryRepository.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly DataContext _dataContext;
+        private readonly IMapper _mapper;
+
+        public CountryRepository(DataContext dataContext, IMapper mapper)
+        {
+            _dataContext = dataContext;
+            _mapper = mapper;
+        }
+        public bool CountryExists(int countryId)
+        {
+            return _dataContext.Countries.Any(c => c.Id == countryId);
+        }
+
+        public bool CreateCountry(Country country)
+        {
+            _dataContext.Add(country);
+            return Save();
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            return _dataContext.Countries.ToList();
+        }
+
+        public Country GetCountry(int id)
+        {
+            return _dataContext.Countries.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public Country GetCountryByOwner(int ownerId)
+        {
+            return _dataContext.Owners.Where(o => o.Id == ownerId).Select(c => c.Country).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnersFromACountry(int countryId)
+        {
+            return _dataContext.Owners.Where(c => c.Country.Id == countryId).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _dataContext.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+    }
+}
